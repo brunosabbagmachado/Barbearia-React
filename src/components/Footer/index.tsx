e3739a6ab173236c46c6a32b26b1ec054cfb9e21
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-09-25 23:29:20 -0300

    começando footer e arrumando pastas

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import 'normalize.css'
+import styles from './Footer.module.scss';
+
+
+export default function Navbar() {
+    return (
+        <footer>
+            
+        </footer>
+    )
+}
--- src/pages/Barbearia/index.tsx
+++ src/pages/Barbearia/index.tsx
@@ -1,12 +1,14 @@
 import 'normalize.css'
-import Navbar from './Navbar';
-import Home from './Main/Home';
+import Navbar from '../../components/Navbar';
+import Home from '../Main/Home';
+import Footer from '../../components/Footer';
 
 export default function Barbearia() {
     return (
         <div>
             <Navbar />
             <Home />
+            <Footer />
         </div>
     )
 }
