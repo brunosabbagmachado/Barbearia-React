1421c54a10e6f3092b64d8325e40813e1a1ffd4d
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-10-06 13:33:59 -0300

    melhorando rotas de links

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import Header from 'components/Header';
-import Footer from 'components/Footer';
+import Navbar from "./components/Navbar";
+import Footer from './components/Footer';
 import Home from 'pages/Home';
 import Sobre from 'pages/Sobre';
+import Produtos from 'pages/Produtos';
+import Contato from 'pages/Contato';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 export default function App() {
     return (
         <>
-            <Header />
-            <Footer />
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path='' element={<Home />} />
+                    <Route path='/sobre' element={<Sobre />} />
+                    <Route path='/produtos' element={<Produtos />} />
+                    <Route path='/contato' element={<Contato />} />
+                </Routes>
+                <Footer />
+            </Router>
         </>
 
     )
